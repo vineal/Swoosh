c0c8c0cfde62566f48092e3d12a8a6810d07abbb
Vineal Viji Varghese <vinealviji@gmail.com>
2020-12-13 00:48:35 +0530

    Made welcome screen and intented to league selection screen.

---
+++ app/src/main/java/com/vineal/swoosh/LeagueSelection.kt
@@ -0,0 +1,11 @@
+package com.vineal.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueSelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league_selection)
+    }
+}
